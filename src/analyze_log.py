3ce0bcd8afbe4ed66b36fdb57cfa5c55f0ed7b88
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-11-14 19:41:16 -0300

    fix lint

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -4,6 +4,7 @@ import os
 alvos = ["maria", "arnaldo", "joao"]
 pratos = ["hamburguer", "pizza", "coxinha", "misto-quente"]
 
+
 def pratos_mais_pedidos(arquivo, alvo):
     prato = {}
 
@@ -13,16 +14,17 @@ def pratos_mais_pedidos(arquivo, alvo):
                 prato[pedido[1]] += 1
             else:
                 prato[pedido[1]] = 1
-    return max(prato, key=prato.get)
 
+    return max(prato, key=prato.get)
 
 def contador_de_pedidos(arquivo, alvo, prato):
     result = 0
+
     for pedido in arquivo:
         if (pedido[0] == alvo and pedido[1] == prato):
             result += 1
-    return result
 
+    return result
 
 def pratos_nunca_pedidos(arquivo, alvo):
     pratos_pedidos = set()
@@ -31,24 +33,24 @@ def pratos_nunca_pedidos(arquivo, alvo):
     for pedido in arquivo:
         if (pedido[0] == alvo):
             pratos_pedidos.add(pedido[1])
-    return pratos_restaurante.difference(pratos_pedidos)
 
+    return pratos_restaurante.difference(pratos_pedidos)
 
 def dias_sem_visitas(arquivo, alvo):
     dias = set()
     dias_visitados = set()
-    
+
     for pedido in arquivo:
         dias.add(pedido[2])
         if (pedido[0] == alvo):
             dias_visitados.add(pedido[2])
-    return dias.difference(dias_visitados) 
 
+    return dias.difference(dias_visitados) 
 
 def analyze_log(path_to_file):
     if ".csv" not in path_to_file:
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
-    
+
     if not os.path.exists(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
@@ -56,7 +58,7 @@ def analyze_log(path_to_file):
         arquivo = list(csv.reader(arquivo, delimiter=',', quotechar='"'))
 
     result = open('data/mkt_campaign.txt', mode='w')
-    
+
     result.write(f"{pratos_mais_pedidos(arquivo, alvos[0])}\n")
     result.write(f"{contador_de_pedidos(arquivo, alvos[1], pratos[0])}\n")
     result.write(f"{pratos_nunca_pedidos(arquivo, alvos[2])}\n")
--- src/track_orders.py
+++ src/track_orders.py
@@ -14,33 +14,32 @@ class TrackOrders:
 
     def get_most_ordered_dish_per_customer(self, customer):
         prato = {}
-        
+    
         for pedido in self.pedidos:
             if pedido["customer"] == customer:
                 if pedido["order"] in prato:
                     prato[pedido["order"]] += 1
                 else:
                     prato[pedido["order"]] = 1
-        
+
         return max(prato, key=prato.get)
 
     def get_never_ordered_per_customer(self, customer):
         pratos_pedidos = set()
-        
+
         for pedido in self.pedidos:
             if (pedido["customer"] == customer):
                 pratos_pedidos.add(pedido["order"])
-        
-        return self.pratos.difference(pratos_pedidos)
 
+        return self.pratos.difference(pratos_pedidos)
 
     def get_days_never_visited_per_customer(self, customer):
         dias_visitados = set()
-    
+
         for pedido in self.pedidos:
             if (pedido["customer"] == customer):
                 dias_visitados.add(pedido["day"])
-        
+
         return self.dias.difference(dias_visitados) 
 
     def get_busiest_day(self):
@@ -51,7 +50,7 @@ class TrackOrders:
                 dias[pedido["day"]] += 1
             else:
                 dias[pedido["day"]] = 1
-        
+
         return max(dias, key=dias.get)
 
     def get_least_busy_day(self):
@@ -62,5 +61,5 @@ class TrackOrders:
                 dias[pedido["day"]] += 1
             else:
                 dias[pedido["day"]] = 1
-        
+
         return min(dias, key=dias.get)
