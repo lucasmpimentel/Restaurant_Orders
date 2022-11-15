eba15177a7c12d565a3b6fbbdca91bccd695a28f
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-11-14 21:27:22 -0300

    req 4.1

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -15,19 +15,33 @@ class InventoryControl:
         'frango': 50,
     }
 
+
     def __init__(self):
         self.ESTOQUE = self.MINIMUM_INVENTORY.copy()
         self.PEDIDOS = list()
+        self.PRATOS_DISPONIVEIS = set()
 
     def add_new_order(self, customer, order, day):
         for ingredient in self.INGREDIENTS[order]:
+            if self.ESTOQUE[ingredient] < 1:
+                return False
             self.ESTOQUE[ingredient] -= 1
         self.PEDIDOS.append((customer, order, day))
 
     def get_quantities_to_buy(self):
-        ordem_de_compra = self.MINIMUM_INVENTORY.copy() 
+        ordem_de_compra = self.MINIMUM_INVENTORY.copy()
 
-        for ingredient in self.ESTOQUE:
-            ordem_de_compra[ingredient] -= self.ESTOQUE[ingredient]
+        for ingrediente in self.ESTOQUE:
+            ordem_de_compra[ingrediente] -= self.ESTOQUE[ingrediente]
 
         return ordem_de_compra
+
+    def get_avaliable_dishes(self):
+        for prato in self.INGREDIENTS:
+            for ingrediente in self.INGREDIENTS[prato]:
+                if self.ESTOQUE[ingrediente] < 1:
+                    break
+                else:
+                    self.PRATOS_DISPONIVEIS.add(prato)
+
+        return self.PRATOS_DISPONIVEIS
